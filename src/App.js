d052eba7712a481500362f759db06aea7da3cdc6
EstiTw <estitwitowork@gmail.com>
2022-08-07 15:14:57 +0300

    rendering movies list

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Movies from "./Components/Movies";
+
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
