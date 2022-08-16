2061498d9fee16e001e99f148d1c6fb47cf8d02f
EstiTw <estitwitowork@gmail.com>
2022-08-16 16:13:52 +0300

    added routing, navbar and movie form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import MovieForm from "./Components/MovieForm";
 import Movies from "./Components/Movies";
+import Customers from "./Components/Customers";
+import Rentals from "./Components/Rentals";
+import NotFound from "./Components/NotFound";
+import Navbar from "./Components/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
